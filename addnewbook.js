e7953f1ea3edba3f8a70087ea2f2d487bab55e63
salma <salma.a.samei@gmail.com>
2024-04-21 02:55:27 +0200

    update

--- addnewbook.js
+++ addnewbook.js
@@ -7,9 +7,28 @@ function savebook() {
         d:document.getElementById("bd").value,
         av:"available"
     };
-    localStorage.setItem("book_"+Date.now(),JSON.stringify(book));
-        alert('Book Added Successfully');
+
+    // console.log(taskList)
+    for (let i = 0; i < localStorage.length; i++) {
+        const key = localStorage.key(i);
+        const task = localStorage.getItem(key);
+        const data = JSON.parse(task);
+        // console.log(data)
+        if (key.startsWith("book_")) {
+            if (data.id === book.id)
+                alert('Duplicated ID Book Cannot be Added');
+            else
+            {
+                localStorage.setItem("book_" + Date.now(), JSON.stringify(book));
+                alert('Book Added Successfully');
+            }
+
+        }
+    }
 }
+
+
+
 document.addEventListener('DOMContentLoaded', function(){
     let taskList = document.getElementById('_book')
     // console.log(taskList)
